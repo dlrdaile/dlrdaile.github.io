9537b1ec8391acfee5a3dd644add7e7d7e733889
dlr <965794928@qq.com>
2024-03-07 01:35:31 +0800

    Site updated: 2024-03-07 01:35:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/26/hello-world/","2024/03/07/程序开发的开闭原则/"];function toRandomPost(){
+var posts=["2024/03/07/程序开发的开闭原则/","2024/02/26/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
